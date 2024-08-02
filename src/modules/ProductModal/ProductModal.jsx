3812086b318116d0a77e8f87301ca8e14dd1e821
AlexeyKonstantinov78 <alex_kav@mail.ru>
2024-08-02 22:11:10 +0300

    feat add adaptive modal

--- src/modules/ProductModal/ProductModal.jsx
+++ src/modules/ProductModal/ProductModal.jsx
@@ -9,11 +9,11 @@ const customStyles = {
     right: 'auto',
     bottom: 'auto',
     marginRight: '-50%',
-    transform: "translate(-50%, -50%)",
-  }
+    transform: 'translate(-50%, -50%)',
+  },
 };
 
-Modal.setAppElement("#root");
+Modal.setAppElement('#root');
 
 export const ProductModal = ({ isOpen, onRequestClose, data }) => {
   if (!data) {
@@ -21,22 +21,61 @@ export const ProductModal = ({ isOpen, onRequestClose, data }) => {
   }
   return (
     <Modal
+      className='modal'
       isOpen={isOpen}
       onRequestClose={onRequestClose}
       style={customStyles}
-      contentLabel="Product Modal"
-    >
-      <h2>{data.title}</h2>
-      <img src={`${API_URL}${data.img}`} alt={data.title} />
-      <p>{data.price}&nbsp;₽</p>
-      <ul>
-        {Object.entries(data.additional).map(([key, value]) => (
-          <li key={key}>
-            <strong>{key}:</strong> {value}
-          </li>
-        ))}
-      </ul>
-      <button onClick={onRequestClose}>Закрыть</button>
+      contentLabel='Product Modal'>
+      <img
+        className='modal__img'
+        src={`${API_URL}${data.img}`}
+        alt={data.title}
+      />
+      <div className='modal__content'>
+        <h2 className='modal__title'>{data.title}</h2>
+        <p className='modal__price'>{data.price}&nbsp;₽</p>
+        <ul className='modal__list'>
+          {Object.entries(data.additional).map(([key, value]) => (
+            <li className='modal__item' key={key}>
+              <strong>{key}:</strong> {value}
+            </li>
+          ))}
+        </ul>
+        <div className="modal__quantity">
+          <div className="cart-item__quantity">
+            <button className="cart-item__quantity-button cart-item__quantity-button_minus"></button>
+            <input className="cart-item__quantity-input" type="number" value={1} />
+            <button className="cart-item__quantity-button cart-item__quantity-button_plus"></button>
+          </div>
+          <button className="cart__order-button">Добавить</button>
+        </div>
+      </div>
+
+      <button className='modal__close' onClick={onRequestClose}>
+        <svg
+          width='20'
+          height='20'
+          viewBox='0 0 20 20'
+          fill='none'
+          xmlns='http://www.w3.org/2000/svg'>
+          <rect
+            x='5.71228'
+            y='14.1975'
+            width='12'
+            height='1.5'
+            transform='rotate(-45 5.71228 14.1975)'
+            fill='#B8B8B8'
+          />
+          <rect
+            x='14.1976'
+            y='15.2582'
+            width='12'
+            height='1.5'
+            transform='rotate(-135 14.1976 15.2582)'
+            fill='#B8B8B8'
+          />
+        </svg>
+      </button>
     </Modal>
   );
 };
