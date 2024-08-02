a6fcd078dee5418032821de05625a874e1d21404
AlexeyKonstantinov78 <alex_kav@mail.ru>
2024-08-02 19:31:44 +0300

    feat add Modal

--- src/modules/Product/Product.jsx
+++ src/modules/Product/Product.jsx
@@ -1,16 +1,32 @@
+import { useState } from 'react';
 import { API_URL } from '../../const';
 import './Product.css';
+import { ProductModal } from '../ProductModal/ProductModal';
 
-export const Product = ({ title, img, price }) => {
+export const Product = ({ title, img, price, additional }) => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const openModal = (event) => {
+    event.preventDefault();
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
   return (
     <li className='products__item'>
-      <article className='product'>
-        <img className='product__img' src={`${API_URL}${img}`} alt={title} />
-        <div className='product__content'>
-          <h3 className='product__title'>{title}</h3>
-          <p className='product__price'>{price}&nbsp;₽</p>
-        </div>
-      </article>
+      <a className='prodict__link' href="#" onClick={openModal} aria-label={`Открыть модальное окно для ${title}`}>
+        <article className='product'>
+          <img className='product__img' src={`${API_URL}${img}`} alt={title} />
+          <div className='product__content'>
+            <h3 className='product__title'>{title}</h3>
+            <p className='product__price'>{price}&nbsp;₽</p>
+          </div>
+        </article>
+      </a>
+      <ProductModal isOpen={modalIsOpen} onRequestClose={closeModal} data={{ title, img, price, additional }} />
     </li>
   );
 };
