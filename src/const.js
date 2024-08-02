39ef604cffede8694d5b9cc81ff060abd1d6441e
AlexeyKonstantinov78 <alex_kav@mail.ru>
2024-08-02 19:52:13 +0300

    feta add title

--- src/const.js
+++ src/const.js
@@ -1,1 +1,9 @@
 export const API_URL = 'https://cup-time-api-m4j8.onrender.com';
+
+export const categoryTitles = [
+  { value: 'tea', title: 'Чай' },
+  { value: 'coffee', title: 'Кофе' },
+  { value: 'teapots', title: 'Чайники' },
+  { value: 'cezves', title: 'Турки' },
+  { value: 'other', title: 'Прочее' },
+];
--- src/modules/Products/Products.jsx
+++ src/modules/Products/Products.jsx
@@ -1,24 +1,30 @@
 import './Products.css';
 import { Product } from '../Product/Product';
 // import { products } from '../../products';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useProducts } from '../context/ProductContext';
 import { useSearchParams } from 'react-router-dom';
+import { categoryTitles } from '../../const';
 
 export const Products = () => {
   const [searchParams] = useSearchParams();
   const { products, setCategory } = useProducts();
   const category = searchParams.get('category');
-
+  const [title, setTitle] = useState("");
 
   useEffect(() => {
     setCategory(category);
+    categoryTitles.forEach((categoryTitle) => {
+      if (categoryTitle.value === category) {
+        setTitle(categoryTitle.title);
+      }
+    });
   }, [category, setCategory]);
 
   return (
     <section className="products">
       <div className="container products__container">
-        <h2 className="products__title">Чай</h2>
+        <h2 className="products__title">{title}</h2>
         <ul className="products__list">
           {
             products.map(product => <Product key={product.id} {...product} />)
