6fd1b16d2aef0fe5759e5111801ebac031f9cd2c
AlexeyKonstantinov78 <alex_kav@mail.ru>
2024-08-04 18:56:06 +0300

    update promo

--- src/modules/Promo/Promo.jsx
+++ src/modules/Promo/Promo.jsx
@@ -1,12 +1,17 @@
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 import './Promo.css';
 
 export const Promo = () => {
+  const [searchParams] = useSearchParams();
+  const category = searchParams.get('category');
+
   return (
     <section className="promo">
       <div className="container promo__container">
         <h1 className="promo__title">Попробуй новый вкус Арабики</h1>
-        <Link className="promo__link" to="/products?category=coffee">Перейти к кофе</Link>
+        {category !== 'coffee' ? (
+          <Link className="promo__link" to="/products?category=coffee">Перейти к кофе</Link>
+        ) : <div className='promo__block'></div>}
       </div>
     </section>
   );
