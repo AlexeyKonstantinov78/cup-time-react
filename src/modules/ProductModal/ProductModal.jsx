d6e14c2a9f079af55a0b9074533f04d1891061d9
AlexeyKonstantinov78 <alex_kav@mail.ru>
2024-08-05 10:46:06 +0300

    update modal

--- src/modules/ProductModal/ProductModal.jsx
+++ src/modules/ProductModal/ProductModal.jsx
@@ -1,20 +1,8 @@
 import Modal from 'react-modal';
-import './ProductModal.css';
 import { API_URL } from '../../const.js';
 import { useState } from 'react';
 import { useCart } from '../../context/CartContext.jsx';
 import _ from './ProductModal.module.css';
-
-const customStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-  },
-};
 
 Modal.setAppElement('#root');
 
